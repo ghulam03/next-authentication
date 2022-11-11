8cf4e6f0cc203d42e8b274aed410f422b100a6b8
ghulam03 <ghulam8905@gmail.com>
2022-11-11 16:24:30 +0530

    first-one

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,57 @@
+import { hashPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
+
+
+//this page logic
+//server side signup form validation
+//duplicate user checks
+//password hashing
+//crud operations on db(store user details on db)
+//error handling
+
+async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return;
+  }
+
+  const data = req.body;
+
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.includes('@') ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res.status(422).json({
+      message:
+        'Invalid input - password should also be at least 7 characters long.',
+    });
+    return;
+  }
+
+  const client = await connectToDatabase();
+
+  const db = client.db();
+
+  const existingUser = await db.collection('users').findOne({ email: email });
+
+  if (existingUser) {
+    res.status(422).json({ message: 'User exists already!' });
+    client.close();
+    return;
+  }
+
+  const hashedPassword = await hashPassword(password);
+
+  const result = await db.collection('users').insertOne({
+    email: email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({ message: 'Created user!' });
+  client.close();
+}
+
+export default handler;
